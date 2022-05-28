50699a7b4e41cb6807718fcdeca7fd958a3cf5df
Asjal <aahmad@trinity.edu>
2022-05-28 15:01:09 -0500

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xff2193b0), Color(0xff2c3e50)])),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Business Card'),
+            backgroundColor: Color(0xff01172F),
+          ),
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/person_img.jpeg'),
+                ),
+                Text(
+                  'Asjal Ahmad',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'STUDENT AT TRINITY UNIVERSITY',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal.shade900),
+                    title: Text(
+                      '(210) 589-0442',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal.shade900),
+                    title: Text(
+                      'aahmad@trinity.edu',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
